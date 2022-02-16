965c7e23d3fc74d364a51fa6df21fc132f4b93e5
Din <dinndev@gmail.com>
2022-02-16 10:33:39 +0800

    :lipstick: Tailwind config files and svg added
    Styles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      montserratBold: ["Montserrat"],
+      robotoSemiBold: ["Roboto"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
