d5a7b0de0b69f55393c2e1cf120b4e274035f71c
Din <dinndev@gmail.com>
2022-03-01 18:13:17 +0800

    :recycle: Relocate the wrapping of global var to app
    fix #1 relocate code from index to app issue fix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
 import Nav from "./Components/Nav";
-import Usercontrol from "./Components/Account/UserControl";
 import ManageExpences from "./Components/Expences/ManageExpences";
-import { BrowserRouter, Link, Routes, Route, Outlet } from "react-router-dom";
-import Login from "./Components/Auth/Login";
+import { TransactionContextProvider } from "./States/TransactionContext";
+import { initialState, reducer } from "./States/reducer";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Signup from "./Components/Auth/Signup";
-import { useEffect } from "react";
-import { useTransactionContext } from "./States/TransactionContext";
+import Usercontrol from "./Components/Account/UserControl";
+import { options } from "./alertOption";
+import Login from "./Components/Auth/Login";
+import PrivateRoute from "./Components/Route/PrivateRoute";
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
 function App() {
   return (
-    <div className="App transition-all flex w-screen">
-      <Nav />
-      <div className="w-4/5  flex m-5">
-        <Outlet />
-      </div>
-    </div>
+    <TransactionContextProvider initialState={initialState} reducer={reducer}>
+      <AlertProvider template={AlertTemplate} {...options}>
+        <BrowserRouter>
+          <div className="App transition-all flex w-screen">
+            <Nav />
+            <div className="w-4/5  flex m-5">
+              <Routes>
+                <Route path="moonbank" element={<Signup />} />
+                <Route
+                  path="moonbank/account"
+                  element={
+                    <PrivateRoute>
+                      <ManageExpences />
+                      <Usercontrol />
+                    </PrivateRoute>
+                  }
+                />
+                <Route path="moonbank/login" element={<Login />} />
+                <Route path="moonbank/signup" element={<Signup />} />
+
+                <Route
+                  path="*"
+                  element={
+                    <main style={{ padding: "1rem" }}>
+                      <p>Bonk! There's nothing here!</p>
+                    </main>
+                  }
+                />
+              </Routes>
+              <Outlet />
+            </div>
+          </div>
+        </BrowserRouter>
+      </AlertProvider>
+    </TransactionContextProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,50 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Usercontrol from "./Components/Account/UserControl";
-import ManageExpences from "./Components/Expences/ManageExpences";
-import Signup from "./Components/Auth/Signup";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./Styles/index.css";
-import { Provider as AlertProvider } from "react-alert";
-import AlertTemplate from "react-alert-template-basic";
-import { TransactionContextProvider } from "./States/TransactionContext";
-import { initialState, reducer } from "./States/reducer";
-import { options } from "./alertOption";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./Components/Auth/Login";
-import PrivateRoute from "./Components/Route/PrivateRoute";
+
 ReactDOM.render(
   <React.StrictMode>
-    <TransactionContextProvider initialState={initialState} reducer={reducer}>
-      <AlertProvider template={AlertTemplate} {...options}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="moonbank" element={<App />}>
-              <Route
-                path="*"
-                element={
-                  <main style={{ padding: "1rem" }}>
-                    <p>Bonk! There's nothing here!</p>
-                  </main>
-                }
-              />
-              <Route path="login" element={<Login />} />
-              <Route path="signup" element={<Signup />} />
-              <Route
-                path="account"
-                element={
-                  <PrivateRoute>
-                    <ManageExpences />
-                    <Usercontrol />
-                  </PrivateRoute>
-                }
-              />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </AlertProvider>
-    </TransactionContextProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
